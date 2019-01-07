be87c803867be22a37265f34dcccb2565a216599
elithecoder <ex.games.org@gmail.com>
2019-01-07 16:07:04 -0600

    added license to server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+//   _______  __   __  _______  ______    _______    __   __  _______  __    _  ______
+//  |       ||  |_|  ||   _   ||    _ |  |       |  |  | |  ||   _   ||  |  | ||      |
+//  |  _____||       ||  |_|  ||   | ||  |_     _|  |  |_|  ||  |_|  ||   |_| ||  _    |
+//  | |_____ |       ||       ||   |_||_   |   |    |       ||       ||       || | |   |
+//  |_____  ||       ||       ||    __  |  |   |    |  |-|  ||       ||  _    || |_|   |
+//   _____| || ||_|| ||   _   ||   |  | |  |   |    |  | |  ||   _   || | |   ||       |
+//  |_______||_|   |_||__| |__||___|  |_|  |___|    |__| |__||__| |__||_|  |__||______|
+//
+//  Created by: EliTheCoder
+//
+//  Copyright (C) 2019 Eli Frigo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
