b4d5bdc6302cdb98dccb6d8c9af923abb4157b6f
EliTheCoder <ex.games.org@gmail.com>
2019-01-17 12:58:47 -0600

    removed rooms for students and removed delay

--- static/student/main.js
+++ static/student/main.js
@@ -3,8 +3,5 @@ let questionEl = document.getElementById("question");
 const socket = io();
 
 function send() {
-  socket.emit("connectroom", pinEl.value);
-  setTimeout(() => {
-    socket.emit("question", {question: questionEl.value, pin: pinEl.value});
-  }, 5000);
+  socket.emit("question", {question: questionEl.value, pin: pinEl.value});
 }
