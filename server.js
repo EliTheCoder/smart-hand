5b48e7f155af55b62c21159b19ba6c373c9a4b37
EliTheCoder <ex.games.org@gmail.com>
2019-01-17 13:59:48 -0600

    removed confusing characters from id generator

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ io.on("connection", socket => {
 // function for generating random codes
 function makeId() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789";
 
   for (var i = 0; i < 5; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
