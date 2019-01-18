58299a2f738cd37b9c547a517404d8889ed1d623
EliTheCoder <ex.games.org@gmail.com>
2019-01-18 10:05:34 -0600

    removed question cooldown was causing problems

--- static/student/main.js
+++ static/student/main.js
@@ -1,18 +1,11 @@
 let pinEl = document.getElementById("class-pin");
 let questionEl = document.getElementById("question");
-let delayed = false;
 const socket = io();
 
 function send() {
-  if (!delayed) {
-    delayed = true;
-    socket.emit("question", {
-      question: questionEl.value,
-      pin: pinEl.value
-    });
-    questionEl.value = "";
-    setTimeout(()=>{
-      delayed = false;
-    },10000);
-  }
+  socket.emit("question", {
+    question: questionEl.value,
+    pin: pinEl.value
+  });
+  questionEl.value = "";
 }
