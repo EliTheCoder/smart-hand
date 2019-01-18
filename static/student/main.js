c2480d5b1bc09492f56af43770e357683167ca47
EliTheCoder <ex.games.org@gmail.com>
2019-01-18 08:57:08 -0600

    added question delay

--- static/student/main.js
+++ static/student/main.js
@@ -1,7 +1,17 @@
 let pinEl = document.getElementById("class-pin");
 let questionEl = document.getElementById("question");
+let delayed = false;
 const socket = io();
 
 function send() {
-  socket.emit("question", {question: questionEl.value, pin: pinEl.value});
+  if (!delayed) {
+    delayed = true;
+    socket.emit("question", {
+      question: questionEl.value,
+      pin: pinEl.value
+    });
+    setTimeout(()=>{
+      delayed = false;
+    },10000);
+  }
 }
