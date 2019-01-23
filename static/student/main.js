7777db02ee7d3c280d95a13a407f06cb74b175d9
EliTheCoder <ex.games.org@gmail.com>
2019-01-23 13:17:51 -0600

    removed cooldown bar

--- static/student/main.js
+++ static/student/main.js
@@ -17,7 +17,6 @@ socket.on("question", data => {
 
 function fullsendfortheboys() {
   if (!delayed) {
-    $("#cooldownbar").width("100%");
     delayed = true;
     socket.emit("question", {
       question: $("#messagebar").val(),
@@ -29,10 +28,3 @@ function fullsendfortheboys() {
     }, 10000);
   }
 }
-
-setInterval(()=>{
-  if ($("#cooldownbar").width() > 0 && delayed) {
-    console.log(Math.round($("#cooldownbar").width()/1440))
-    $("#cooldownbar").width(Math.round($("#cooldownbar").width()/1440) - 1 + "%");
-  }
-}, 100);
